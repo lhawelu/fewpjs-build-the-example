23463c35576128ffb0844d9c14e230db2c1a4205
Larry Hawelu <lhawelu@gmail.com>
2021-03-11 17:29:55 -0800

    commit tree 44a3901b6ff938032b760e6c74eae78b9104783d

--- main.js
+++ main.js
@@ -3,8 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
+const likeButtons = document.querySelectorAll('.like-glyph')
+likeButtons.textContent = EMPTY_HEART
+const errorModal = document.querySelector('#modal')
+for (const likeButton of likeButtons) {
+  likeButton.addEventListener('click', e => {
+    if (likeButton.textContent === EMPTY_HEART) {
+      mimicServerCall(e)
+      .then((e) => {
+        likeButton.classList.add('activated-heart')
+        likeButton.textContent = FULL_HEART
+      })
+      .catch(error => {
+        errorModal.classList.remove('hidden')
+        errorModal.textContent = 'Error -' + error
+        setTimeout(() => {
+          errorModal.classList.add('hidden')
+        }, 5000)
+      })
+    } else {
+      likeButton.classList.remove('activated-heart')
+      likeButton.textContent = EMPTY_HEART
+    }
+  })
+}
 
 
 //------------------------------------------------------------------------------
